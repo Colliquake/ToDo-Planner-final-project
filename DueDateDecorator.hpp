446068631238caca33af7f8116ecc70bfd0bc531
ChauAbdul786 <achau048@ucr.edu>
2020-11-24 20:05:38 -0800

    Fixes #8

---
+++ DueDateDecorator.hpp
@@ -0,0 +1,28 @@
+#ifndef __DUE_DATE_DECORATOR__
+#define __DUE_DATE_DECORATOR__
+#include"Decorator.hpp"
+using namespace std;
+class DueDateDecorator : public Decorator {
+public:
+	DueDateDecorator();
+	DueDateDecorator(TodoList*);
+	DueDateDecorator(TodoList*, int, int, int);
+	void setDueMonth(int);
+	void setDueDay(int);
+	void setDueYear(int);
+	int getDueMonth();
+	int getDueDay();
+	int getDueYear();
+
+	virtual void setStatus(bool inp);
+	virtual void add(TodoList* inp);
+	virtual void display();
+	virtual bool status();
+private:
+	TodoList* todoList;
+	int dueMonth;
+	int dueDay;
+	int dueYear;
+};
+
+#endif
